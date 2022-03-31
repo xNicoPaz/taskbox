9aa881700ba0671c24c1be2b0af078a2bc63061c
Nicolas Paz <nicolas.paz.sde@gmail.com>
2022-03-31 20:40:22 -0300

    feat: custom stories, injecting css, snapshot testing with storyshot

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,7 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
