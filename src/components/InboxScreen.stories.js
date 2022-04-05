719aeaf6d040dfa022484fc9be8539efdac9e731
Nicolas Paz <nicolas.paz.sde@gmail.com>
2022-04-05 19:54:05 -0300

    feat: screen stories, API mocking  & interactions

---
+++ src/components/InboxScreen.stories.js
@@ -0,0 +1,58 @@
+import { fireEvent, waitForElementToBeRemoved, within } from "@storybook/testing-library";
+import { waitFor } from "@testing-library/react";
+import { rest } from "msw";
+import { Provider } from "react-redux";
+import store from "../lib/store";
+import InboxScreen from "./InboxScreen";
+import { MockedState } from "./TaskList.stories";
+
+export default {
+  component: InboxScreen,
+  title: 'InboxScreen',
+  decorators: [
+    (story) => <Provider store={store}>{story()}</Provider>
+  ]
+}
+
+const Template = () => <InboxScreen />
+
+export const Default = Template.bind({});
+Default.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=1',
+        (req, res, ctx) => {
+          return res(ctx.json(MockedState.tasks));
+        }
+      )
+    ]
+  }
+}
+
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Waits for the component to transition from the loading state
+  await waitForElementToBeRemoved(await canvas.findByTestId('loading'));
+  // Waits for the component to be updated based on the store
+  await waitFor(async () => {
+    // Simulates pinning the first task
+    await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+    // Simulates pinning the third task
+    await fireEvent.click(canvas.getByLabelText('pinTask-3'));
+  })
+}
+
+export const Error = Template.bind({});
+Error.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=1',
+        (req, res, ctx) => {
+          return res(ctx.status(403));
+        }
+      ),
+    ],
+  },
+};
